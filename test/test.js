b4de80a05b9e296a1bb254fbcedd92e443402d1e
Ivan Nikolić <ivan.nikolic@trikoder.net>
2017-05-26 09:54:10 +0200

    Add "name" property when in development mode
    Closes #1.

--- test/test.js
+++ test/test.js
@@ -26,3 +26,31 @@ test('Generic functional component', t => {
 };`
   )
 })
+
+test('Generic functional export with "name" when NODE_ENV === "development"', t => {
+  const nodeEnv = process.env.NODE_ENV
+  process.env.NODE_ENV = 'development'
+  t.is(
+    transpile(`export const a = ({ props, listeners }) => <div onClick={listeners.click}>{props.msg}</div>`),
+    `export const a = {
+  name: "a",
+  functional: true,
+  render: (h, { props, listeners }) => <div onClick={listeners.click}>{props.msg}</div>
+};`
+  )
+  process.env.NODE_ENV = nodeEnv
+})
+
+test('Generic functional component with "name" when NODE_ENV === "development"', t => {
+  const nodeEnv = process.env.NODE_ENV
+  process.env.NODE_ENV = 'development'
+  t.is(
+    transpile(`const a = ({ props, listeners }) => <div onClick={listeners.click}>{props.msg}</div>`),
+    `const a = {
+  name: "a",
+  functional: true,
+  render: (h, { props, listeners }) => <div onClick={listeners.click}>{props.msg}</div>
+};`
+  )
+  process.env.NODE_ENV = nodeEnv
+})
